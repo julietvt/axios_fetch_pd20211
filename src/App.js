3ddcc1620ecf663d7d11e82f4b79370f9f4c4f98
julietvt <freshitpt@gmail.com>
2021-12-04 10:01:38 +0200

    fetch example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+const options = {
+  results: 10,
+  page: 1,
+  seed: 'PD2021-1',
+};
+
+function loadUsers({ results, seed, page }) {
+  fetch(
+    `https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`
+  )
+    .then((response) => response.json())
+    .then(({ results }) => console.dir(results))
+    .catch((error) => console.log(error));
+}
+
 function App() {
-  return (
-    <p>Load data from randomuser</p>
-  );
+  loadUsers(options);
+  return <p>Load data from randomuser</p>;
 }
 
 export default App;
