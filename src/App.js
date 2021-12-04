6474aade060c6a3983e2e9f7879aed91805c7224
julietvt <freshitpt@gmail.com>
2021-12-04 10:33:40 +0200

    error treatment

--- src/App.js
+++ src/App.js
@@ -7,20 +7,30 @@ const options = {
   seed: 'PD2021-1',
 };
 
+const configAxios = {
+  onUploadProgress: (event) => console.log(event.loaded),
+  onDownloadProgress: (event) => console.log(event.loaded),
+  timeout: 2000,
+};
+
 function loadUsersAxios({ results, seed, page }) {
   axios
     .get(
-      `https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`
+      `https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`,
+      configAxios
     )
     .then((response) => console.log(response.data.results))
-    .catch((error) => console.log(error));
+    .catch((error) => console.log(error.message));
 }
 
 function loadUsers({ results, seed, page }) {
   fetch(
     `https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`
   )
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) throw Error(response.statusText);
+      return response.json();
+    })
     .then(({ results }) => console.dir(results))
     .catch((error) => console.log(error));
 }
